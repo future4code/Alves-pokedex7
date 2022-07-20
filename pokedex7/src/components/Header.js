5b23c5b9351ca7e53c3ff784bbd4c849d447d0c5
João Gabriel Colodetti do Carmo <jgcolodetti13@hotmail.com>
2022-07-20 15:20:42 -0300

    pokedex e detalhes de estilização, responsividade

--- pokedex7/src/components/Header.js
+++ pokedex7/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Title from '../img/header.png'
 import { useNavigate } from 'react-router-dom'
@@ -13,6 +13,10 @@ align-items: center;
 justify-content: center;
 background-color: white;
 gap: 450px;
+@media (min-width: 365px) and (max-width: 763px){ 
+    grid-template-columns: 0px 33% 33%;
+    gap: 0%;
+}
 `
 
 const ImageHeader = styled.img`
@@ -20,15 +24,21 @@ width: 70%;
 :hover { 
   cursor: pointer;
 }
+justify-self: center;
+@media (min-width: 365px) and (max-width: 763px){ 
+  width: 90%;
+}
 `
 
 const PokedexButton = styled.button`
+width: 250px;
+height: 60px;
+justify-self: center;
 display: flex;
 flex-direction: row;
 justify-content: center;
 align-items: center;
 padding: 8px;
-top: 41px;
 background: #33A4F5;
 border-radius: 8px;
 border: 0;
@@ -42,15 +52,34 @@ opacity: 0.9;
   background: #33A4F5;
   opacity: 1;
 }
+@media (min-width: 365px) and (max-width: 763px){ 
+  padding: 2px;
+  font-size: 16px;
+  width: 70%;
+  height: 40px;
+}
 `
 
 export default function Header() {
+  const [onHome, setOnHome] = useState(true)
   const navigate = useNavigate()
+  const goToPokedex = () => {
+    setOnHome(false)
+    goToPokedexPage(navigate)
+  }
+
+  const returnHome = () => {
+    setOnHome(true)
+    goToHomePage(navigate)
+  }
   return (
     <Main>
       <div></div>
       <ImageHeader src={Title} onClick={() => goToHomePage(navigate)}/>
-      <PokedexButton onClick={() => goToPokedexPage(navigate)}>Pokedéx</PokedexButton>
+      {onHome ? 
+        <PokedexButton onClick={goToPokedex}>Pokedéx</PokedexButton> :
+        <PokedexButton onClick={returnHome}>Home</PokedexButton>
+      }
     </Main>
   )
 }
--- pokedex7/src/components/PokeCard.js
+++ pokedex7/src/components/PokeCard.js
@@ -1,8 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react'
 import styled, { css } from 'styled-components'
-import Pngwing2 from '../img/pngwing2.png'
 import TypeGrass from '../img/grass.png'
+import TypeBug from '../img/bug.png'
+import TypeWater from '../img/water.png'
+import TypeFire from '../img/fire.png'
+import TypeNormal from '../img/normal.png'
+import TypeFlying from '../img/flying.png'
+import TypePoison from '../img/poison.png'
 import { GlobalContext } from './global/GlobalContext'
+import { useNavigate } from 'react-router-dom'
+import { goToDetailsPage } from '../routes/coordinator'
 
 
 const Main = styled.div`
@@ -11,6 +18,11 @@ width: 440px;
 height: 263px;
 flex-direction: column;
 justify-content: flex-end;
+color: white;
+@media (min-width: 365px) and (max-width: 763px){ 
+  width: 100%;
+  height: 240px;
+}
 `
 
 const Container = styled.div`
@@ -36,8 +48,19 @@ ${({ color }) => {
       case 'normal':
         return css`
         background-color: #BF9762;`
+      case 'poison':
+        return css`
+        background-color: #BF9762;`
+      case 'flying':
+        return css`
+        background-color: #95D1FC;`
     }
   }}    
+  @media (min-width: 365px) and (max-width: 763px){ 
+  width: 100%;
+  height: 263px;
+}
+  
 `
 
 const TopContainer = styled.div`
@@ -46,6 +69,10 @@ width: 100%;
 height: 80%;
 justify-content: space-between;
 align-items:center;
+@media (min-width: 365px) and (max-width: 763px){ 
+  justify-content: center;
+}
+
 `
 
 const TopDiv = styled.div`
@@ -53,6 +80,10 @@ display: flex;
 flex-direction: column;
 justify-content: flex-start;
 height: 100%;
+@media (min-width: 365px) and (max-width: 763px){ 
+  height: 60%;
+}
+
 `
 const BottomDiv = styled.div`
 display: flex;
@@ -66,6 +97,11 @@ const Image = styled.img`
 width: 200px;
 height: 200px;
 margin-bottom: 105px;
+@media (min-width: 365px) and (max-width: 763px){ 
+  margin-bottom: 0;
+  width: 100px;
+  height: 100px;
+}
 `
 
 const Image2 = styled.img`
@@ -96,7 +132,37 @@ opacity: 0.9;
   cursor: pointer;
   opacity: 1;
 }
+@media (min-width: 365px) and (max-width: 763px){ 
+  margin-left: 150px;
+}
 `
+
+const ButtonExcluir = styled.button`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 4px 10px;
+width: 146px;
+height: 38px;
+background: #FF6262;
+border-radius: 8px;
+border: 0px;
+position: absolute;
+margin-left: 275px;
+font-weight: 700;
+font-size: 1rem;
+font-family: 'Poppins';
+opacity: 0.8;
+:hover { 
+  cursor: pointer;
+  opacity: 1;
+}
+@media (min-width: 365px) and (max-width: 763px){ 
+  margin-left: 200px;
+}
+`
+
 const ButtonDetalhes = styled.button`
 width: 74px;
 height: 24px;
@@ -131,8 +197,14 @@ ${({ color }) => {
       case 'normal':
         return css`
         background-color: #BF9762;`
+      case 'poison':
+        return css`
+        background-color: #BF9762;`
+      case 'flying':
+        return css`
+        background-color: #95D1FC;`
     }
-  }}   
+  }}    
 `
 
 const IdText = styled.p`
@@ -145,6 +217,7 @@ padding-left: 20px;
 const TypeImage = styled.img`
 width: 18px;
 height: 18px;
+
 `
 
 const NameText = styled.h2`
@@ -165,20 +238,48 @@ flex-direction: row;
 align-items: flex-start;
 padding: 5px 8px;
 gap: 10px;
-background-color: lightpink;
 border-radius: 8px;
+background-color: #729F92;
+${({ color }) => {
+    switch (color) {
+      case 'grass':
+        return css`
+        background-color: #70B873;`
+      case 'water':
+        return css`
+        background-color: #33A4F5;`
+      case 'fire':
+        return css`
+        background-color: #F44900;`
+      case 'bug':
+        return css`
+        background-color: #316520;`
+      case 'normal':
+        return css`
+        background-color: #8A8A8A;`
+      case 'poison':
+        return css`
+        background-color: #AD61AE;`
+      case 'flying':
+        return css`
+        background-color: #6892B0;`
+    }
+  }}  
 `
 const TypeText = styled.p`
 margin: 0;
 font-size: 0.8rem;
+font-weight:700;
 `
-export default function PokeCard({ name, image, id, types }) {
+export default function PokeCard({ name, image, id, types, captured }) {
   const [color, setColor] = useState('')
-
-  const { onCapture } = useContext(GlobalContext)
+  const [color2, setColor2] = useState('')
+  const { onCapture, onDelete } = useContext(GlobalContext)
+  const navigate = useNavigate()
 
   useEffect(() => {
     const type = types[0]
+    const type2 = types[1]
     if (type === 'grass') {
       setColor('grass')
     } else if (type === 'water') {
@@ -189,8 +290,28 @@ export default function PokeCard({ name, image, id, types }) {
       setColor('bug')
     } else if (type === 'normal') {
       setColor('normal')
+    } else if (type === 'poison') {
+      setColor('poison')
+    } else if (type === 'flying') {
+      setColor('flying')
     }
-    console.log(type, color)
+
+    if (type2 === 'grass') {
+      setColor2('grass')
+    } else if (type2 === 'water') {
+      setColor2('water')
+    } else if (type2 === 'fire') {
+      setColor2('fire')
+    } else if (type2 === 'bug') {
+      setColor2('bug')
+    } else if (type2 === 'normal') {
+      setColor2('normal')
+    } else if (type2 === 'poison') {
+      setColor2('poison')
+    } else if (type2 === 'flying') {
+      setColor2('flying')
+    }
+    console.log(type)
   })
 
 
@@ -201,22 +322,50 @@ export default function PokeCard({ name, image, id, types }) {
           <TopDiv>
             <IdText>#{id}</IdText>
             <NameText>{name}</NameText>
-            <TypesDiv>
-              <TypeContainer>
-                <TypeImage src={TypeGrass} />
-                <TypeText>Grass</TypeText>
-              </TypeContainer>
-              <TypeContainer>
-                <TypeImage src={TypeGrass} />
-                <TypeText>Grass</TypeText>
-              </TypeContainer>
-            </TypesDiv>
+            {types.length === 1 ?
+              <TypesDiv>
+                <TypeContainer color={color}>
+                  {(color === 'grass') ? <TypeImage src={TypeGrass} /> : null}
+                  {(color === 'water') ? <TypeImage src={TypeWater} /> : null}
+                  {(color === 'fire') ? <TypeImage src={TypeFire} /> : null}
+                  {(color === 'normal') ? <TypeImage src={TypeNormal} /> : null}
+                  {(color === 'bug') ? <TypeImage src={TypeBug} /> : null}
+                  {(color === 'poison') ? <TypeImage src={TypePoison} /> : null}
+                  {(color === 'flying') ? <TypeImage src={TypeFlying} /> : null}
+                  <TypeText>{types[0].toUpperCase()}</TypeText>
+                </TypeContainer>
+              </TypesDiv>
+              :
+              <TypesDiv>
+                <TypeContainer color={color}>
+                  {(color === 'grass') ? <TypeImage src={TypeGrass} /> : null}
+                  {(color === 'water') ? <TypeImage src={TypeWater} /> : null}
+                  {(color === 'fire') ? <TypeImage src={TypeFire} /> : null}
+                  {(color === 'normal') ? <TypeImage src={TypeNormal} /> : null}
+                  {(color === 'bug') ? <TypeImage src={TypeBug} /> : null}
+                  {(color === 'poison') ? <TypeImage src={TypePoison} /> : null}
+                  {(color === 'flying') ? <TypeImage src={TypeFlying} /> : null}
+                  <TypeText>{types[0].toUpperCase()}</TypeText>
+                </TypeContainer>
+                <TypeContainer color={color2}>
+                  {(color2 === 'grass') ? <TypeImage src={TypeGrass} /> : null}
+                  {(color2 === 'water') ? <TypeImage src={TypeWater} /> : null}
+                  {(color2 === 'fire') ? <TypeImage src={TypeFire} /> : null}
+                  {(color2 === 'normal') ? <TypeImage src={TypeNormal} /> : null}
+                  {(color2 === 'bug') ? <TypeImage src={TypeBug} /> : null}
+                  {(color2 === 'poison') ? <TypeImage src={TypePoison} /> : null}
+                  {(color2 === 'flying') ? <TypeImage src={TypeFlying} /> : null}
+                  <TypeText>{types[1].toUpperCase()}</TypeText>
+                </TypeContainer>
+              </TypesDiv>
+            }
           </TopDiv>
           <Image src={image} />
         </TopContainer>
         <BottomDiv>
-          <ButtonDetalhes color={color}>Details</ButtonDetalhes>
-          <ButtonCapturar onClick={() => onCapture(name, image, id, types)}>Capturar!</ButtonCapturar>
+          <ButtonDetalhes color={color} onClick={() => goToDetailsPage(navigate)}>Details</ButtonDetalhes>
+          {captured ? <ButtonExcluir onClick={() => onDelete(id)}>Soltar</ButtonExcluir> :
+            <ButtonCapturar onClick={() => onCapture(name, image, id, types, true)}>Capturar!</ButtonCapturar>}
         </BottomDiv>
       </Container>
     </Main>
